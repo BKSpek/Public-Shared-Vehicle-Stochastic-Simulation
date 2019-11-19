97023dbc9d0f00c102d445f7e11287bb071cb890
Allan Spektor <allanspektor@yahoo.com>
2019-11-18 19:54:31 -0500

    hw4_q1_b_DES

--- hw4_q1_b_DES/hw4_q1_b_DES.cpp
+++ hw4_q1_b_DES/hw4_q1_b_DES.cpp
@@ -1,14 +1,21 @@
 /*
-	A)
-	Output after 100 runs:
-
-	not actuall DES atm, poisson generator for number events over period
-	exponential generator with some rate = 1 / lambda or something like that (distance between events)
+	Output after 100 trials:
 	...
-	Total Money at the end of experiment 365.85
-	Total Money at the end of experiment 384.1
-	Total Money at the end of experiment 375.35
-	Average amount of money over 10000 iterations : 361.763
+	Total Time Spent with no bikes during trial 30.389
+	Total Time Spent with no bikes during trial 26.5569
+	Total Time Spent with no bikes during trial 28.264
+	Average amount of money over 100 iterations : 360.05
+	Average time spent with no bikes over 100 iterations : 28.8555
+	Average cost of dissatisfaction over 100 iterations : -93.7804 +-2.9717
+
+	Output after 10000 trials:
+	...
+	Total Time Spent with no bikes during trial 35.6788
+	Total Time Spent with no bikes during trial 28.8811
+	Total Time Spent with no bikes during trial 33.8833
+	Average amount of money over 10000 iterations : 361.017
+	Average time spent with no bikes over 10000 iterations : 29.0693
+	Average cost of dissatisfaction over 10000 iterations : -94.4752 +-0.296064
 */
 
 #include <iostream>
@@ -54,9 +61,14 @@ int main()
 	std::exponential_distribution<double> type2Clock(clientRates[2]);
 	std::exponential_distribution<double> type3Clock(clientRates[3]);
 
-	const int numberOfTrials = 10000;
+	const int numberOfTrials = 100;
 	double averageMoneyAmount = 0;
 	unsigned long numberOfEvents = 0;
+
+	double averageCostOfDissatisfaction = 0;
+	double averageTimeWithNoBikes = 0;
+
+	std::vector<double> costValues;
 
 	std::cout << "Starting the trials" << std::endl;
 
@@ -65,6 +77,9 @@ int main()
 		//we can assume total money starts at 0 + the deterministic annual prorated charge of clients classes 1 and 2
 		double totalMoney = (0.5 * clientRates[1]) + (0.1 * clientRates[2]);
 		int bikeCount = 10; //we start with 10 bikes at X(0)
+
+		double timeSpentWithNoBikes = 0;
+		double startOfNoBikes = -1;
 
 		//events
 		std::map<double, int> events; //holds arrival time, type
@@ -95,6 +110,13 @@ int main()
 			if (eventType == 0) //a bike has arrived
 			{
 				bikeCount++; //increment bike amount
+			
+				//if was in state where no bikes, end the interval and record the delta
+				if (startOfNoBikes != -1)
+				{
+					timeSpentWithNoBikes += eventTime - startOfNoBikes; //record the time interval with no bikes
+					startOfNoBikes = -1; //set to -1 to indicate that we are not in a state of no bikes
+				}
 			}
 			else //a client has arrived
 			{
@@ -108,6 +130,12 @@ int main()
 					}
 					//decrement the bike count
 					bikeCount--;
+
+					//if no more bikes start the timer
+					if (bikeCount == 0)
+					{
+						startOfNoBikes = eventTime;
+					}
 				}
 				else
 				{
@@ -117,15 +145,36 @@ int main()
 			}
 		}
 
-		std::cout << "Total Money at the end of experiment " << totalMoney << std::endl;
+		//std::cout << "Total Money at the end of experiment " << totalMoney << std::endl;
 		averageMoneyAmount += totalMoney;
+
+		//record the cost for sample variance calculation
+		costValues.push_back((timeSpentWithNoBikes * clientRates[1] * clientPenalty[1]) + (timeSpentWithNoBikes * clientRates[2] * clientPenalty[2]));
+
+		//record for global mean calculation [further down]
+		std::cout << "Total Time Spent with no bikes during trial " << timeSpentWithNoBikes << std::endl;
+		averageTimeWithNoBikes += timeSpentWithNoBikes;
 	}
 
 	std::cout << "Average amount of money over " << numberOfTrials << " iterations" << " : "
 		<< (averageMoneyAmount / numberOfTrials) << std::endl;
 
-	std::cout << "Average amount of events over " << numberOfTrials << " iterations" << " : "
-		<< (numberOfEvents / numberOfTrials) << std::endl;
+	/*std::cout << "Average amount of events over " << numberOfTrials << " iterations" << " : "
+		<< (numberOfEvents / numberOfTrials) << std::endl;*/
+
+	std::cout << "Average time spent with no bikes over " << numberOfTrials << " iterations" << " : "
+		<< (averageTimeWithNoBikes / numberOfTrials) << std::endl;
+
+	double globalMeanOfCost = (((averageTimeWithNoBikes / numberOfTrials) * clientRates[1] * clientPenalty[1]) + ((averageTimeWithNoBikes / numberOfTrials) * clientRates[2] * clientPenalty[2]));
+
+	double sampleVariance = calculateSampleVariance(costValues, globalMeanOfCost);
+
+	double z = 1.96;
+
+	double CI = z * (sampleVariance / sqrt(costValues.size()));
+
+	std::cout << "Average cost of dissatisfaction over " << numberOfTrials << " iterations" << " : "
+		<< globalMeanOfCost << " +-" << CI << std::endl;
 
 	std::getchar();
 
